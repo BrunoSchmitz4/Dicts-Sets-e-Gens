5cab3c948ceb0909bd2c3ea6983dbae4152eb11a
Bruno Silva <brunosch250@gmail.com>
2023-07-10 13:50:30 -0300

    Learning LambdaExpression

--- DictAppOne/DictAppOne/Program.cs
+++ DictAppOne/DictAppOne/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DictAppOne.Entities;
+using System;
 using System.Globalization;
 
 namespace DictAppOne
@@ -7,8 +8,30 @@ namespace DictAppOne
     {
         static void Main(string[] args)
         {
-            SeconndExtensionMethods();
+            ProductInit();
         }
+
+        static void ProductInit()
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV", 900.00));
+            list.Add(new Product("Notebook", 1200.00));
+            list.Add(new Product("Tablet", 450.00));
+
+            // Ordena lista (tipo as coleções Sorted :))
+            list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()););
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        //static int CompareProducts(Product p1, Product p2)
+        //{
+        //    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
+        //}
 
         static void SeconndExtensionMethods()
         {
