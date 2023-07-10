bc4e7c62cea75db368aa993815ffa5cb8f19c26e
Bruno Silva <brunosch250@gmail.com>
2023-07-10 11:01:03 -0300

    Initial commit

---
+++ DictAppOne/DictAppOne/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DictAppOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> cookies = new Dictionary<string, string>();
+
+            // Dict | Key   |  Value
+            cookies["User"] = "Maria";
+            cookies["Email"] = "maria@gmail.com";
+            cookies["Phone"] = "995623482";
+            cookies["Phone"] = "580217552";
+
+            cookies.Remove("Email");
+
+            if (!(cookies.ContainsKey("Email")))
+            {
+                Console.WriteLine("A chave email não existe.");
+            }
+            else
+            {
+                Console.Write(cookies["Email"]);
+            }
+
+            Console.WriteLine("----------------------");
+            Console.WriteLine("\nTodos os cookies: ");
+            Console.WriteLine("\nQuantidade: " + cookies.Count);
+            Console.WriteLine("\nKey / Value \n");
+            
+            foreach (KeyValuePair<string, string> kvp in cookies)
+            {
+                
+                Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+            }
+        }
+    }
+}
