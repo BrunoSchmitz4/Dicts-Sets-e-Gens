c2f4fdf88b807fdb2c59971ba787b744c3dee576
Bruno Silva <brunosch250@gmail.com>
2023-07-10 11:44:32 -0300

    Learning Extensions Methods

--- DictAppOne/DictAppOne/Program.cs
+++ DictAppOne/DictAppOne/Program.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using System.Globalization;
 
 namespace DictAppOne
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            SeconndExtensionMethods();
+        }
+
+        static void SeconndExtensionMethods()
+        {
+            string s1 = "Hello dear traveller! May u journey be full of good memories and wonderfull experiences!";
+            Console.WriteLine("Mensagem para você: \n" +
+                $"{s1.Cut(14)}");
+        }
+
+        static void FirstExtensionMethods()
+        {
+            //DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            DateTime dt = new DateTime(2023, 7, 10, 2, 00, 00);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+        static void LearningDictionary()
         {
             Dictionary<string, string> cookies = new Dictionary<string, string>();
 
@@ -28,10 +47,10 @@ namespace DictAppOne
             Console.WriteLine("\nTodos os cookies: ");
             Console.WriteLine("\nQuantidade: " + cookies.Count);
             Console.WriteLine("\nKey / Value \n");
-            
+
             foreach (KeyValuePair<string, string> kvp in cookies)
             {
-                
+
                 Console.WriteLine($"{kvp.Key}: {kvp.Value}");
             }
         }
